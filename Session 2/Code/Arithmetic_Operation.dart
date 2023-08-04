c181cfd06b5f2b567109643e67867d8c031e8fc7
Ahmed Magdy <ahmedmagdy2319@gmail.com>
2023-08-04 21:30:22 +0200

    add session 1 and 2

---
+++ Session 2/Code/Arithmetic_Operation.dart
@@ -0,0 +1,25 @@
+void main(){
+  
+ int num1 = 10;
+ int num2 = 3;
+
+ int result = num1 + num2;
+ print(result); // prints: "13"
+
+ result = num1 - num2;
+ print(result); //prints:"7"
+
+ result = num1 * num2;
+ print(result); //prints:"30"
+
+ result = num1 ~/ num2 ;
+ print(result);//prints : "3", integer division
+
+ result = num2 % num1;
+ print(result); //prints: "1"
+
+ double result_double = num1 / num2;
+ print (result_double); //prints :"3.333..."
+
+
+}
---
+++ Session 2/Code/data_type.dart
@@ -0,0 +1,45 @@
+void main(){
+
+// String data type
+String name = "Ahmed";
+
+// int data type
+int age =21;
+
+
+// double data type
+double price = 1000.50;
+
+
+// num data type carry int or double
+num num1 = 2;
+num num2 = 2.5;
+
+
+// bool data type
+bool isTrue= true ;
+
+
+// dynamic data type
+dynamic value = 'Ahmed';
+value = 5;
+value = 5.5;
+value = true;
+
+
+
+// var not data type
+var myValue ='Ali Osama';
+
+
+
+print('My Name is : ${name}');
+
+print(age);
+
+
+
+
+
+
+}
